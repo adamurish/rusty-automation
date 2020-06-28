44aad302aa565ae47e6a4f5b2e5b4ccdf56128aa
Adam <adamurish@gmail.com>
2020-06-28 15:22:45 -0500

    server

--- basic_node/src/main.rs
+++ basic_node/src/main.rs
@@ -8,24 +8,27 @@ use rocket_contrib::json::Json;
 use std::process::Command;
 
 use rppal::gpio::Gpio;
+use std::thread;
+use std::time::Duration;
+use rocket::State;
 
 #[get("/on")]
-fn on() -> String {
-    let mut pin = Gpio::new().unwrap().get(26).unwrap().into_output();
+fn on(gpio: State<Gpio>) -> String {
+    let mut pin = gpio.get(26).unwrap().into_output();
     pin.set_high();
     format!("LED on")
 }
 
 #[get("/off")]
-fn off() -> String {
-    let mut pin = Gpio::new().unwrap().get(26).unwrap().into_output();
+fn off(gpio: State<Gpio>) -> String {
+    let mut pin = gpio.get(26).unwrap().into_output();
     pin.set_low();
     format!("LED off")
 }
 
 fn main() {
-//    let rocket_lobster = rocket::ignite();
-//    rocket_lobster.mount("/", routes![on, off]).launch();
-    let mut pin_test = Gpio::new().unwrap().get(26).unwrap().into_output();
-    pin_test.set_high();
+    let rocket_lobster = rocket::ignite();
+    rocket_lobster.mount("/", routes![on, off]);
+    rocket_lobster.manage(Gpio::new().unwrap());
+    rocket_lobster.launch();
 }
